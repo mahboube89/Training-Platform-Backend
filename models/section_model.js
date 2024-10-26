07a6f26f6e992f25593d7b755f9ab68352c1f35c
mahboube89 <mahboube89r@gmail.com>
2024-10-26 19:00:20 +0200

    feat: Add model for section

---
+++ models/section_model.js
@@ -0,0 +1,41 @@
+/**
+ * @Copyright 2024 mae-mahboube89
+ * @license MIT
+*/
+
+"use strict";
+
+// ----- Node modules -----
+const mongoose = require('mongoose');
+
+const SectionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLenght: 5,
+        maxLenght: 100
+    },
+    video: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    free: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    tutorialId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tutorial",
+        required: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Section", SectionSchema)
