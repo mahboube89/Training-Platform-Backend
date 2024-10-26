2eb35603e37dca45ed495c173ced1ef19773cf7b
mahboube89 <mahboube89r@gmail.com>
2024-10-26 20:39:55 +0200

    refactor: Improve Tutorial model structure

--- models/tutorial_model.js
+++ models/tutorial_model.js
@@ -7,8 +7,9 @@
 
 // ----- Node modules -----
 const mongoose = require('mongoose');
+const { schema } = require('./user_model');
 
-const TutorialSchema = new mongoose.Schema( {
+const TutorialSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
@@ -22,12 +23,55 @@ const TutorialSchema = new mongoose.Schema( {
         trim: true,
         maxLength: 500
     },
+    cover: {
+        type: String,
+        required: true
+    },
     instructor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
+    },
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+    href: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    status: {
+        type: String,
+        enum: ["Compelete", "Incomplete"],
+        required: true
+    },
+    onSale: {
+        type: Boolean,
+        default: false
     }
 
 }, {timestamps: true});
+
+
+// Virtual for Sections linked to the tutorial
+TutorialSchema.virtual("sections", {
+    ref: "Section",
+    localField: "_id",
+    foreignField: "tutorialId"
+});
+
+
+// Virtual for Comments linked to the tutorial
+schema.virtual("comments", {
+    ref: "Comment",
+    localField: "_id",
+    foreignField: "tutorialId"
+});
 
 module.exports = mongoose.model("Tutorial", TutorialSchema);
